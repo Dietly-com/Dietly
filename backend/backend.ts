e630fb8db0023ac0e852a9024fc46362b494d41e
DESKTOP-QVSO6G1\MIXU <michaxplaygames@gmail.com>
2022-10-22 18:24:01 +0200

    DIETLY-5 - update endpoints

--- backend/backend.ts
+++ backend/backend.ts
@@ -11,6 +11,12 @@ const userBadgeRoute = require('./src/routes/UserBadgeRoute');
 const userMealProductRoute = require('./src/routes/UserMealProductRoute');
 const userMealRoute = require('./src/routes/UserMealRoute');
 const userPersonalBestRoute = require('./src/routes/UserPersonalBestRoute');
+const unitRoute = require('./src/routes/UnitRoute');
+const nutrientRoute = require('./src/routes/NutrientRoute');
+const productRoute = require('./src/routes/ProductRoute');
+const productNutrientRoute = require('./src/routes/ProductNutrientRoute');
+const recipeRoute = require('./src/routes/RecipeRoute');
+const recipeProductRoute = require('./src/routes/RecipeProductRoute');
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -26,7 +32,12 @@ app.use('/userBadge', userBadgeRoute);
 app.use('/userMealProduct', userMealProductRoute);
 app.use('/userMeal', userMealRoute);
 app.use('/userPersonalBest', userPersonalBestRoute);
-
+app.use('/unit',unitRoute);
+app.use('/nutrient',nutrientRoute);
+app.use('/product',productRoute);
+app.use('/productNutrient',productNutrientRoute);
+app.use('/recipe',recipeRoute);
+app.use('/recipeProduct',recipeProductRoute);
 
 app.listen(port, () => {
     console.log(`⚡️[Backend]: Server is running at http://localhost:${port}`);
---
+++ backend/src/routes/RecipeProduct.ts
@@ -0,0 +1,31 @@
+import express, { Router } from 'express';
+import {verifyUser } from '../middlewares/UserMiddleware';
+import {createOne, findOne, findMany, updateOne, deleteOne } from '../controllers/StandardController';
+import { PrismaClient} from '@prisma/client';
+
+const object = new PrismaClient().recipeProduct;
+const router: Router = express.Router();
+
+router.use(verifyUser);
+
+router.post("/", async (req, res) => {
+    createOne(req, res, object);
+})
+
+router.get("/:id",  async (req, res) => {
+    findOne(req, res, object);
+})
+
+router.get("/",  async (req, res) => {
+    findMany(req, res, object);
+})
+
+router.patch("/",  async (req, res) => {
+    updateOne(req, res, object);
+})
+
+router.delete("/:id",  async (req, res) => {
+    deleteOne(req, res, object);
+})
+
+module.exports = router
