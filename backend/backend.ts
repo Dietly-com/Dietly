cd7b421d5eb79b6e3993166fe562bab151bf6a83
jakubLabedowicz <jalabedowicz@gmail.com>
2022-10-22 17:57:18 +0200

    DIETLY-4 - add endpoints

--- backend/backend.ts
+++ backend/backend.ts
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const bodyParser=require('body-parser');
 const port = 8080;
+
 const userRoute = require('./src/routes/UserRoute');
 const fileRoute = require('./src/routes/FileRoute');
+const badgeRoute = require('./src/routes/BadgeRoute');
+const personalBestRoute = require('./src/routes/PersonalBestRoute');
+const userBadgeRoute = require('./src/routes/UserBadgeRoute');
+const userMealProductRoute = require('./src/routes/UserMealProductRoute');
+const userMealRoute = require('./src/routes/UserMealRoute');
+const userPersonalBestRoute = require('./src/routes/UserPersonalBestRoute');
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -13,6 +20,12 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.use('/user', userRoute);
 app.use('/file', fileRoute);
+app.use('/user', badgeRoute);
+app.use('/personalBest', personalBestRoute);
+app.use('/userBadge', userBadgeRoute);
+app.use('/userMealProduct', userMealProductRoute);
+app.use('/userMeal', userMealRoute);
+app.use('/userPersonalBest', userPersonalBestRoute);
 
 
 app.listen(port, () => {
