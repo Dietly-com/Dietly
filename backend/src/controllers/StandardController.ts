8c86b010de64daa8d67f6c14d2d42d6e997010d5
jakubLabedowicz <jalabedowicz@gmail.com>
2022-10-22 20:18:10 +0200

    DIETLY-8 - fix

--- backend/src/controllers/StandardController.ts
+++ backend/src/controllers/StandardController.ts
@@ -24,26 +24,40 @@ export const updateOne = async (req: Request, res: Response, object:any) => {
     }
 }
 
-export const findOne = async (req: Request, res: Response, object:any, include:any = {}) => {
+export const findOne = async (req: Request, res: Response, object:any, include:any = undefined) => {
     try {
-        const { id } = req.params
-        const record = await object.findOne({
-            where: {
-                id: Number(id),
-            },
-            include: include,
-        })
+        const { id } = req.params;
+        let record = null;
+        if(include != undefined) {
+            record = await object.findUnique({
+                where: {
+                    id: Number(id),
+                },
+                include: include,
+            })
+        } else {
+            record = await object.findUnique({
+                where: {
+                    id: Number(id),
+                },
+            })
+        }
         res.status(201).send({data: record, success: true, message: "Znaleziono rekord"});
     } catch (error) {
         res.status(500).send({ success: false, message: "Wewnętrzny błąd serwera"});
     }
 }
 
-export const findMany = async (req: Request, res: Response, object:any, include:any = {}) => {
+export const findMany = async (req: Request, res: Response, object:any, include:any = undefined) => {
     try {
-        const records = await object.findMany({
-            include: include
-        });
+        let records = null;
+        if(include != undefined) {
+            records = await object.findMany({
+                include: include
+            });
+        } else {
+            records = await object.findMany();
+        }
         res.status(201).send({data: records, success: true, message: "Znaleziono rekordy"});
     } catch (error) {
         res.status(500).send({ success: false, message: "Wewnętrzny błąd serwera"});
