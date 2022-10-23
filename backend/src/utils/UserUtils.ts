7429b9f7d5c938ae68c19d1fe6b68e4dd1cf988a
DESKTOP-QVSO6G1\MIXU <michaxplaygames@gmail.com>
2022-10-23 14:12:27 +0200

    DIETLY-9 - hotfix

--- backend/src/utils/UserUtils.ts
+++ backend/src/utils/UserUtils.ts
@@ -1,9 +1,12 @@
-import jwt from 'jsonwebtoken';
+import jwt,{Secret} from 'jsonwebtoken';
 import dotenv from 'dotenv';
 dotenv.config();
 
 export const generateAccessToken = (id: Number) => {
-    const tokenSecret: string = process.env.TOKEN_SECRET as string;
+    const tokenSecret: Secret = process.env.TOKEN_SECRET as Secret;
+    console.log(id);
+    console.log(tokenSecret);
+    console.log(jwt.sign(id, tokenSecret, { expiresIn: '7d' }));
     return jwt.sign(id, tokenSecret, { expiresIn: '7d' });
 }
 
