d12d35941ad4f595c2f39084d0d4268b506861e8
jakubLabedowicz <jalabedowicz@gmail.com>
2022-10-25 21:46:11 +0200

    DIETLY-13 - change UserMiddleware to AuthMiddleware

---
+++ backend/src/middlewares/AuthMiddleware.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from 'express';
+import { verifyAccessToken } from '../utils/TokenUtils';
+import {
+  ResponseBuilder,
+  STATUS_UNAUTHORIZED,
+  MESSAGE_WRONG_TOKEN
+} from '../utils/ResponseUtils';
+
+export const verifyUser = async (req: Request, res: Response, next: NextFunction) => {
+  const auth = req.headers.authorization;
+  if (auth && auth.startsWith('Bearer')) {
+    const token = auth.slice(7);
+
+    try {
+      const tokenData = verifyAccessToken(token);
+      req.body.tokenData = tokenData;
+      next();
+    } catch (error) {
+      res = new ResponseBuilder(res)
+      .withStatus(STATUS_UNAUTHORIZED)
+      .withResponseBodySuccess(false)
+      .withResponseBodyMessage(MESSAGE_WRONG_TOKEN)
+      .send();
+    }
+  } else {
+    res = new ResponseBuilder(res)
+    .withStatus(STATUS_UNAUTHORIZED)
+    .withResponseBodySuccess(false)
+    .withResponseBodyMessage(MESSAGE_WRONG_TOKEN)
+    .send();
+  }
+}
