722d4776c334366290066e6d94ac29de88528b17
Jaman2 <junipohsde@gmail.com>
2022-11-04 12:21:17 +0100

    DIETLY-50 - Fixed backend crash when given wrong login data.

--- backend/src/services/AuthorizationService.ts
+++ backend/src/services/AuthorizationService.ts
@@ -10,69 +10,64 @@ import {
     MESSAGE_LOGGED_USER,
     MESSAGE_INTERNAL_SERVER_ERROR,
     MESSAGE_WRONG_LOGIN_DATA
-  } from '../utils/ResponseUtils';
+} from '../utils/ResponseUtils';
 
 const prisma = new PrismaClient();
 const Joi = require("joi");
 const bcrypt = require("bcrypt");
 
-export const authUser = async (req:Request, res:Response) => {
+export const authUser = async (req: Request, res: Response) => {
     try {
         const schema = Joi.object({
             login: Joi.string().required().label("Login"),
             password: Joi.string().required().label("Password"),
         })
         const { error } = schema.validate(req.body.data);
-        if (error){
-            console.log(error);
-            return res = new ResponseBuilder(res)
-            .withStatus(STATUS_BAD_REQUEST)
-            .withResponseBodySuccess(false)
-            .withResponseBodyMessage({
-                text: error.details[0].message,
-                type: 'error',
-                code: 'E0'
-            })
-            .send();
-        }
-        
-        const user = await prisma.user.findUnique({
-            where: {
-              login: req.body.data.login,
-            },
-          });
-        if (!user){
-            return res = new ResponseBuilder(res)
-            .withStatus(STATUS_UNAUTHORIZED)
-            .withResponseBodySuccess(false)
-            .withResponseBodyMessage(MESSAGE_WRONG_LOGIN_DATA)
-            .send();
-        }
-            
-        const validPassword = await bcrypt.compare(
-            req.body.data.password,
-            user.password
-        )
-        if (!validPassword){
+        if (error) {
+            throw MESSAGE_WRONG_LOGIN_DATA;
+        } else {
+            const user = await prisma.user.findUnique({
+                where: {
+                    login: req.body.data.login,
+                },
+            });
+            let validPassword: any;
+            if (!user) {
+                throw MESSAGE_WRONG_LOGIN_DATA;
+            } else {
+                validPassword = await bcrypt.compare(
+                    req.body.data.password,
+                    user.password
+                )
+                if (!validPassword) {
+                    throw MESSAGE_WRONG_LOGIN_DATA;
+                }
+
+            }
+
+            const token = generateAccessToken(user.id);
             res = new ResponseBuilder(res)
-            .withStatus(STATUS_UNAUTHORIZED)
-            .withResponseBodySuccess(false)
-            .withResponseBodyMessage(MESSAGE_WRONG_LOGIN_DATA)
-            .send();
+                .withStatus(STATUS_OK)
+                .withResponseBodyData({ token: token, user: user })
+                .withResponseBodySuccess(true)
+                .withResponseBodyMessage(MESSAGE_LOGGED_USER)
+                .send();
+
         }
-            
-        const token = generateAccessToken(user.id);
-        res = new ResponseBuilder(res)
-        .withStatus(STATUS_OK)
-        .withResponseBodyData({token: token, user: user})
-        .withResponseBodySuccess(true)
-        .withResponseBodyMessage(MESSAGE_LOGGED_USER)
-        .send();
+
     } catch (error) {
-        res = new ResponseBuilder(res)
-        .withStatus(STATUS_INTERNAL_SERVER_ERROR)
-        .withResponseBodySuccess(false)
-        .withResponseBodyMessage(MESSAGE_INTERNAL_SERVER_ERROR)
-        .send();
+        if (error == MESSAGE_WRONG_LOGIN_DATA) {
+            res = new ResponseBuilder(res)
+                .withStatus(STATUS_UNAUTHORIZED)
+                .withResponseBodySuccess(false)
+                .withResponseBodyMessage(MESSAGE_WRONG_LOGIN_DATA)
+                .send();
+        } else {
+            res = new ResponseBuilder(res)
+                .withStatus(STATUS_INTERNAL_SERVER_ERROR)
+                .withResponseBodySuccess(false)
+                .withResponseBodyMessage(MESSAGE_INTERNAL_SERVER_ERROR)
+                .send();
+        }
     }
 }
