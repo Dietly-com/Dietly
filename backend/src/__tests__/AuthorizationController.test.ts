3e1bab624d6f8d29dc155f97ab97beb46500fc51
Jaman2 <junipohsde@gmail.com>
2022-11-20 22:05:34 +0100

    DIETLY-77 - Reworked tests, created tests for auth diet and badge endpoints.

--- backend/src/__tests__/AuthorizationController.test.ts
+++ backend/src/__tests__/AuthorizationController.test.ts
@@ -1,30 +1,45 @@
-import { describe, it} from '@jest/globals';
+import { describe, it, beforeAll, afterAll, expect } from '@jest/globals';
 import { app } from '../../backend_test';
-const stest = require("supertest");
+let request = require('supertest')(app);
 
-describe("Authorization",() => {
-    describe("Given no login or password",() => {
-        it("Should return 401", () => {
-            stest(app).post('/api/v1/auth').expect(401);
-        })
+describe("Given no login and password", () => {
+    it("Should return 401", async () => {
+        const temp = await request.post('/api/v1/auth')
+        expect(temp.status).toBe(401)
     })
-    describe("Given bad login",() => {
-        it("Should return 401", () => {
-            stest(app).post('/api/v1/auth').send({
-                "data":{
-                    "login":"66as66ads6bt8vbv6t8as6t8d"
+})
+
+describe("Given bad password", () => {
+    it("Should return 401", async () => {
+        const temp = await request.post('/api/v1/auth')
+            .send({
+                "data": {
+                    login: "test",
+                    password: "badPassword"
                 }
-            }).expect(401);
-        })
+            })
+
+        expect(temp.status).toBe(401)
     })
-    describe("Given good login",() => {
-        it("Should return 200", () => {
-            stest(app).post('/api/v1/auth').send({
-                "data":{
-                    "login":"admin",
-                    "password":"admin"
-                }
-            }).expect(200);
+})
+
+describe("Given good login", () => {
+    it("Should return 200", async () => {
+        const temp = await request.post('/api/v1/auth').send({
+            "data": {
+                login: "test",
+                password: "PasswordForTesting"
+            }
         })
+
+        expect(temp.status).toBe(200)
+    })
+})
+
+describe("Given no token", () => {
+    it("Should return 401", async () => {
+        const temp = await request
+            .post('/api/v1/badge')
+        expect(temp.status).toBe(401)
     })
 })
