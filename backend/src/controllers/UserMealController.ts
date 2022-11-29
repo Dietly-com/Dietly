efd8e7d5d286eaf218056d2cdbb4e0c9e30ac2d0
Jaman2 <junipohsde@gmail.com>
2022-11-29 22:44:11 +0100

    DIETLY-77 - Added more test for yet another controllers, though some of them dont work due to date issues.

--- backend/src/controllers/UserMealController.ts
+++ backend/src/controllers/UserMealController.ts
@@ -1,6 +1,6 @@
 import express, { Router } from 'express';
-import {createOne, findOne, findMany, updateOne, deleteOne } from '../services/StandardService';
-import { PrismaClient} from '@prisma/client';
+import { createOne, findOne, findMany, updateOne, deleteOne } from '../services/StandardService';
+import { PrismaClient } from '@prisma/client';
 import { verifyUser } from '../middlewares/AuthorizationMiddleware';
 import { RequestBuilder } from '../utils/RequestUtils';
 import { addWhere } from '../middlewares/WhereMiddleware';
@@ -10,36 +10,56 @@ import { getUserMealsNutrients, getUserMealWithNutrients } from '../utils/Nutrie
 
 const object = new PrismaClient().userMeal;
 const include = {
-    userMealProducts: {include: {
-        product: {include: {
-            file: true,
-            unit: true,
-            productNutrients: {include: {
-                nutrient: {include: {
-                    unit: true
-                }}
-            }}
-        }},
-        unit:true
-    }},
-    userMealRecipes: {include: {
-        recipe: {include: {
-            file: true,
-            unit: true,
-            recipeProducts: {include: {
-                product: {include: {
+    userMealProducts: {
+        include: {
+            product: {
+                include: {
                     file: true,
-                    productNutrients: {include: {
-                        nutrient: {include: {
-                            unit: true
-                        }}
-                    }}
-                }}
-            }}
-        }}, 
-        unit:true
-    }},
-    user:true
+                    unit: true,
+                    productNutrients: {
+                        include: {
+                            nutrient: {
+                                include: {
+                                    unit: true
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            unit: true
+        }
+    },
+    userMealRecipes: {
+        include: {
+            recipe: {
+                include: {
+                    file: true,
+                    unit: true,
+                    recipeProducts: {
+                        include: {
+                            product: {
+                                include: {
+                                    file: true,
+                                    productNutrients: {
+                                        include: {
+                                            nutrient: {
+                                                include: {
+                                                    unit: true
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            unit: true
+        }
+    },
+    user: true
 };
 
 const router: Router = express.Router();
@@ -49,8 +69,8 @@ router.use(addPagination);
 router.use(addOrder);
 router.use(async (req, res, next) => {
     req = new RequestBuilder(req)
-    .withInclude(include)
-    .get();
+        .withInclude(include)
+        .get();
     next();
 })
 
@@ -58,12 +78,14 @@ let findNutrientsResponseBodyDataProcessor = (responseBodyData: any) => {
     responseBodyData.data = getUserMealsNutrients(responseBodyData.data);
     return responseBodyData;
 }
-router.get("/nutrients/",  async (req, res) => {
+router.get("/nutrients/", async (req, res) => {
     findMany(req, res, object, findNutrientsResponseBodyDataProcessor);
 })
 
 router.post("/", async (req, res) => {
-    req.body.data.userId = req.body.authorization.id;
+    if (req.body.data) {
+        req.body.data.ownerId = req.body.authorization.id;
+    }
     createOne(req, res, object);
 })
 
@@ -71,19 +93,19 @@ let findOneResponseBodyDataProcessor = (responseBodyData: any) => {
     responseBodyData.data = getUserMealWithNutrients(responseBodyData.data);
     return responseBodyData;
 }
-router.get("/:id",  async (req, res) => {
+router.get("/:id", async (req, res) => {
     findOne(req, res, object, findOneResponseBodyDataProcessor);
 })
 
-router.get("/",  async (req, res) => {
+router.get("/", async (req, res) => {
     findMany(req, res, object);
 })
 
-router.patch("/:id",  async (req, res) => {
+router.patch("/:id", async (req, res) => {
     updateOne(req, res, object);
 })
 
-router.delete("/:id",  async (req, res) => {
+router.delete("/:id", async (req, res) => {
     deleteOne(req, res, object);
 })
 
